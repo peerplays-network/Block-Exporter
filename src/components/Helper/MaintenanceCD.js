bd261f603c7b226ae2d90eacc67ee4be17538a5a
Matthew Hutchinson <Zeke725@users.noreply.github.com>
2018-09-19 11:36:29 -0300

    maintenance countdown file made and added

---
+++ src/components/Helper/MaintenanceCD.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Countdown from 'react-countdown-now';
+import styles from './styles.css';
+
+class MaintenanceCD extends Component {
+	
+	render() {
+		return (
+			<div>
+				<div className={`${styles['center']} card border-dark mb-3`} style={this.props.style}>
+					<div className="card-header pr-1">
+						<span>
+							<h5 className="card-title float-left">{this.props.headerText}</h5>
+							<button className={`${styles['closeButton']} link float-right border-0 bg-transparent`} onClick={this.props.onClose}>X</button>
+						</span>
+					</div>
+					<div className={`${styles['box-shadow']} p-3 ${styles['centerText']}`}>
+                        <Countdown date={Date.now() + 10000} />
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default MaintenanceCD;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Button } from 'reactstrap';
 import Panel from '../Helper/Panel';
+import MaintenanceCD from '../Helper/MaintenanceCD';
 import styles from './styles.css';
 import {Rnd} from 'react-rnd';
 
@@ -8,7 +9,7 @@ class Welcome extends Component {
 
 	onClosePanel() {
     	console.log('Close Clicked');
-  	}
+  }
 
 	render() {
 		return (
@@ -16,12 +17,33 @@ class Welcome extends Component {
 				<Rnd
 				  default={{
 				    x: 200,
-				    y: 200,
+				    y: 50,
 				    width: 320,
 				    height: 200,
 				  }}
 				>
-					<Panel headerText={'Transition Feed'} width={'400px'} style={{  margin: '24px auto', width: '400px' }} onClose={this.onClosePanel.bind(this)}>
+					<MaintenanceCD 
+						headerText={'Maintenance Countdown'} 
+						width={'400px'} 
+						style={{ margin: '24px auto', width: '400px' }} 
+						onClose={this.onClosePanel.bind(this)} 
+					/>
+				</Rnd>
+
+				<Rnd
+				  default={{
+				    x: 200,
+				    y: 300,
+				    width: 320,
+				    height: 200,
+				  }}
+				>
+					<Panel 
+						headerText={'Transition Feed'} 
+						width={'400px'} 
+						style={{ margin: '24px auto', width: '400px' }} 
+						onClose={this.onClosePanel.bind(this)}
+						>
 						<div className={`${styles['data-react']}`}>
 							<h3>Test</h3>
 							<Button>Bootstrap Button</Button>
