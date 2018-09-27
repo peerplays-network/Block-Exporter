1ea21a6068322c4f29c414dff30869a282f2a8a7
Matthew Hutchinson <Zeke725@users.noreply.github.com>
2018-09-27 10:04:45 -0300

    style update

--- src/components/Transactions/TransactionRow.js
+++ src/components/Transactions/TransactionRow.js
@@ -5,12 +5,16 @@ class TransactionRow extends Component {
 
 	render() {
 		return (
-            <div>
-                <span className={`${styles['blue']}`}>{this.props.account} &nbsp;</span>
-                <span>{this.props.action} &nbsp;</span>
-                <span className={`${styles['green']}`}>{this.props.memo} &nbsp;</span>
-                <span className={`${styles['lightred']}`}>{this.props.time} &nbsp;</span>
-                <span className={`${styles['grey']}`} style={{float:'right'}}>{this.props.transactionID}</span>
+            <div className="container" style={{margin:'0px', padding:'0px'}}>
+                <div className="col-10" style={{float:'left', textAlign: 'left', margin:'0px', padding:'0px'}}>
+                    <span className={`${styles['blue']}`}>{this.props.account} &nbsp;</span>
+                    <span>{this.props.action} &nbsp;</span>
+                    <span className={`${styles['green']}`}>{this.props.memo} &nbsp;</span>
+                    <span className={`${styles['lightred']}`}>{this.props.time} &nbsp;</span>
+                </div>
+                <div className="col-2" style={{float:'right', textAlign: 'right', margin:'0px', padding:'0px'}}>
+                    <span className={`${styles['grey']}`}>{this.props.transactionID}</span>
+                </div>
             </div>
 		);
 	}
