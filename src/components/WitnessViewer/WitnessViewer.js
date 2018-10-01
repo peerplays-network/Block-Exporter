02b6ab1c09245b081977e87b8e450e56d3c0a573
Adrian <ametzler@exeblock.com>
2018-10-01 16:00:08 -0300

    added different height for small to medium

--- src/components/WitnessViewer/WitnessViewer.js
+++ src/components/WitnessViewer/WitnessViewer.js
@@ -5,17 +5,18 @@ import axios from 'axios';
 class WitnessViewer extends Component {
 	constructor() {
 		super();
-		this.state = {witnessData: []};
+		this.state = {topFiveWitnessData: [], witnessData: []};
 	}
 
 	fetchData() {
-		//API call to search for Account
+		//API call to search for witness
 		axios.get('api/witnesses/', {
 		}).then(response => {
 			let sortedWitnessData = response.data.sort((a, b) => (a.total_votes > b.total_votes) ? 1 : ((b.total_votes > a.total_votes) ? -1 : 0));
+			this.setState({witnessData: sortedWitnessData});
 			sortedWitnessData = sortedWitnessData.slice(0, 5);
-			this.setState({ witnessData: sortedWitnessData });
-		}).catch(error => {console.log('error fetching account data', error);});
+			this.setState({ topFiveWitnessData: sortedWitnessData });
+		}).catch(error => {console.log('error fetching witness data', error);});
 	}
 
 	componentDidMount() {
@@ -23,33 +24,72 @@ class WitnessViewer extends Component {
 		const gridHeight=40;
 		this.props.calculateComponentHeight(this.props.id, gridHeight);
 	}
+
+	renderSmallTable() {
+		return (
+			<table className="table">
+				<thead className="thead-light">
+					<tr>
+						<th scope="col">Rank</th>
+						<th scope="col">Witness</th>
+						<th scope="col">Votes</th>
+						<th scope="col">Misses</th>
+						<th scope="col">URL</th>
+					</tr>
+				</thead>
+				<tbody>
+					{this.state.topFiveWitnessData.map((witness, index) => {
+						return <WitnessRow
+							key={witness.id}
+							rank={index+1}
+							witness={witness.account_name}
+							votes={witness.total_votes}
+							misses={witness.total_missed}
+							lastBlock={witness.url}
+						/>;
+					})}
+				</tbody>
+			</table>
+		);
+	}
+
+	renderBigTable() {
+		return (
+			<table className="table">
+				<thead className="thead-light">
+					<tr>
+						<th scope="col">Rank</th>
+						<th scope="col">Witness</th>
+						<th scope="col">Votes</th>
+						<th scope="col">Misses</th>
+						<th scope="col">URL</th>
+					</tr>
+				</thead>
+				<tbody>
+					{this.state.witnessData.map((witness, index) => {
+						return <WitnessRow
+							key={witness.id}
+							rank={index+1}
+							witness={witness.account_name}
+							votes={witness.total_votes}
+							misses={witness.total_missed}
+							lastBlock={witness.url}
+						/>;
+					})}
+				</tbody>
+			</table>
+		);
+	}
 	
 	render() {
+		console.log('witness data: ', this.state.witnessData);
 		return (
 			<div>
-				<table className="table">
-					<thead className="thead-light">
-						<tr>
-							<th scope="col">Rank</th>
-							<th scope="col">Witness</th>
-							<th scope="col">Votes</th>
-							<th scope="col">Misses</th>
-							<th scope="col">URL</th>
-						</tr>
-					</thead>
-					<tbody>
-						{this.state.witnessData.map((witness, index) => {
-							return <WitnessRow
-								key={witness.id}
-								rank={index+1}
-								witness={witness.account_name}
-								votes={witness.total_votes}
-								misses={witness.total_missed}
-								lastBlock={witness.url}
-							/>;
-						})}
-					</tbody>
-				</table>
+				{this.props.size === 'small' ? 
+					this.renderSmallTable()
+					:
+					this.renderBigTable()
+				}
 			</div>
 		);
 	}
