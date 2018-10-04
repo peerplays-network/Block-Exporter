cfad041574457153bb2a395bcfd4883e2634f746
Prabhjot Singh <psingh@exeblock.com>
2018-10-04 16:23:40 -0300

    Page Layout for Fee Directory

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -72,6 +72,9 @@ export default class NavBar extends React.Component {
 							<NavItem>
 								<NavLink tag={RRNavLink} to="/account/">Account</NavLink>
 							</NavItem>
+							<NavItem>
+								<NavLink tag={RRNavLink} to="/feeDirectory">Fee Directory</NavLink>
+							</NavItem>
 						</Nav>
 					</Collapse>
 				</Navbar>
--- src/routes/MainRoutes.js
+++ src/routes/MainRoutes.js
@@ -6,6 +6,7 @@ import Home from '../components/Home';
 import Test from '../components/Test';
 import Account from '../components/Account';
 import AccountAllDetail from '../components/Account/AccountAllDetail';
+import FeeDirectory from '../components/FeeDirectory/FeeDirectory';
 
 export default (
 	<div>
@@ -15,6 +16,7 @@ export default (
 			<Route exact path="/test" component={Test} />
 			<Route exact path="/account" component={Account} />
 			<Route exact path="/accountAllDetail/*" component={AccountAllDetail} />
+			<Route exact path="/feeDirectory" component={FeeDirectory} />
 		</Switch>
 	</div>
 );
