e9e2553f8872e1cd7634dbd12ddbf7e0bfaad243
admin <admin@AlexandersMBP3.datasecuritynode.com>
2018-10-05 15:57:44 -0300

    adding to the webpage

--- src/components/Account/AccountAllDetail.js
+++ src/components/Account/AccountAllDetail.js
@@ -1,15 +1,19 @@
 /*Used in Account*/
 import React,  { Component } from 'react';
-import { Button, UncontrolledCollapse } from 'reactstrap';
+import { Nav, NavItem, NavLink, Row, Col, TabContent, TabPane, Card, CardBody } from 'reactstrap';
 import axios from 'axios';
+import classnames from 'classnames';
 
 class AccountAllDetail extends Component {
 	constructor(e) {
 		super();
 		this.state = {
 			data: [],
-			transactions: []
+			transactions: [],
+			witnesses: [],
+			activeTab: '1'
 		};
+		this.toggle = this.toggle.bind(this);
 		this.account = '';
 		this.findData = this.findData.bind(this);
 		this.findTransactions = this.findTransactions.bind(this);
@@ -40,47 +44,98 @@ class AccountAllDetail extends Component {
 		});
 	}
 
+	findWitnesses(e) {
+		const account = this.account;
+		this.getAccount();
+		//API call to search for Account
+		axios.get('/api/witnesses/', {
+		}).then(response => {
+			const sortedWitnessData = response.data.filter(function(item) {
+				return item.account_name === account;
+			 });
+			this.setState({ witnesses: sortedWitnessData });
+		}).catch(error => {console.log('error is fetching account data', error);});
+	}
+
 	componentDidMount() {
 		this.findData();
 		//this.findTransactions();
+		this.findWitnesses();
+	}
+
+	toggle(tab) {
+		if (this.state.activeTab !== tab) {
+			this.setState({
+				activeTab: tab
+			});
+		}
+	}
+
+	tabNavBuild( index, type) {
+		return (
+			<NavItem>
+				<NavLink className={classnames({ active: this.state.activeTab === index })} onClick={() => { this.toggle(index); }}>
+					{ type } Details ({ this.account })
+				</NavLink>
+			</NavItem>
+		);
 	}
 
 	render() {
-		const toggler = 'toggler'+this.props.account;
-		const togglerCol = '#toggler'+this.props.account;
-
 		this.getAccount();
 
 		return (
+
 			<div>
-				<Button color="primary" id={toggler} style={{ marginBottom: '1rem' }}>
-					More details about { this.account }
-				</Button>
-				{this.state.data.map((account, i) =>
-					<UncontrolledCollapse toggler={togglerCol} key={i} >
-						<a> Name: { account.account_name } </a>
-						<a>Account ID: { account.account_id } </a>
-						<a>Account Key: { account.active_key } </a><br />
-						<a>ID: { account.id } </a>
-						<a>Referrer ID: { account.referrer } </a><br />
-						<a>Member Since: { account.member_since } </a><br />
-						<a>Membership Expires: { account.membership_expiration }</a><br />
-						<a>Memo Key: { account.memo_key } </a>
-						<a>Owner Key: { account.owner_key } </a>
-						<a>Fees Paid: { account.lifetime_fees_paid } </a>
-					</UncontrolledCollapse>
-				)}
-				<br />
-				<Button color="primary" id={toggler+'trans'} style={{ marginBottom: '1rem' }}>
-					Transactions { this.account }
-				</Button>
-				{this.state.transactions.map((transaction, i) =>
-					<UncontrolledCollapse toggler={togglerCol+'trans'} key={i} >
-						<a> Name: { transaction.account_name } </a>
-						<a>Transacitons ID: { transaction.account_id } </a>
-						<a>Account Key: { transaction.active_key } </a>
-					</UncontrolledCollapse>
-				)}
+				<Nav tabs>
+					{ this.tabNavBuild(1, 'Account') }
+					{ this.tabNavBuild(2, 'Transactions') }
+					{ this.tabNavBuild(3, 'Witness') }
+					<NavItem>
+						<NavLink className={classnames({ active: this.state.activeTab === '3' })} onClick={() => { this.toggle('3'); }}>
+							Witness Details ({ this.account })
+						</NavLink>
+					</NavItem>
+				</Nav>
+				<Card>
+					<CardBody>
+						<TabContent activeTab={this.state.activeTab}>
+							<TabPane tabId="1">
+								<h4>Account Information</h4>
+								{this.state.data.map((account, i) =>
+									<Row key={i}>
+										<Col sm="2"> Name: <strong>{ account.account_name }</strong></Col>
+										<Col sm="2"> Account ID: <strong>{ account.account_id }</strong></Col>
+										<Col sm="2"> Lifetime fees paid: <strong>{ account.lifetime_fees_paid }</strong></Col>
+										<Col sm="2"> Registrar: <strong>{ account.referrer }</strong></Col>
+									</Row>
+								)}
+							</TabPane>
+							<TabPane tabId="2">
+								<h4>Transactions</h4>
+								{this.state.transactions.map((transaction, i) =>
+									<Row key={i}>
+										<Col sm="2"> Name: <strong>{ transaction.account_name }</strong></Col>
+										<Col sm="2"> Account ID: <strong>{ transaction.account_id }</strong></Col>
+										<Col sm="2"> Lifetime fees paid: <strong>{ transaction.lifetime_fees_paid }</strong></Col>
+										<Col sm="2"> Registrar: <strong>{ transaction.referrer }</strong></Col>
+									</Row>
+								)}
+							</TabPane>
+							<TabPane tabId="3">
+								<h4>Witness Information</h4>
+								{this.state.witnesses.map((witness, i) =>
+									<Row key={i}>
+										<Col sm="2"> Name: <strong>{ witness.account_name }</strong></Col>
+										<Col sm="2"> Account ID: <strong>{ witness.account_id }</strong></Col>
+										<Col sm="2"> Lifetime fees paid: <strong>{ witness.lifetime_fees_paid }</strong></Col>
+										<Col sm="2"> Registrar: <strong>{ witness.referrer }</strong></Col>
+									</Row>
+								)}
+							</TabPane>
+						</TabContent>
+					</CardBody>
+				</Card>
 			</div>
 			
 		);
