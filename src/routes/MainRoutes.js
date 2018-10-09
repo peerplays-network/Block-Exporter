b4750541c3833fc79960419ecc8f254a7645808e
admin <admin@AlexandersMBP3.datasecuritynode.com>
2018-10-09 13:51:13 -0300

    simple directory

--- src/routes/MainRoutes.js
+++ src/routes/MainRoutes.js
@@ -9,6 +9,7 @@ import AccountAllDetail from '../components/Account/AccountAllDetail';
 import BlockView from '../components/BlockView/BlockView';
 import TransactionLarge from '../components/Transactions/TransactionLarge';
 import FeeDirectory from '../components/FeeDirectory/FeeDirectory';
+import Directory from '../components/Directory/Directory';
 
 export default (
 	<div>
@@ -20,6 +21,7 @@ export default (
 			<Route exact path="/accountAllDetail/*" component={AccountAllDetail} />
 			<Route exact path="/transactions" component={TransactionLarge} />
 			<Route exact path="/feeDirectory" component={FeeDirectory} />
+			<Route exact path="/directory" component={Directory} />
 			<Route exact path="/block-view" component={BlockView} />
 		</Switch>
 	</div>
