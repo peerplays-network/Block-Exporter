566cd70f1a361f96ce0c318c4ce00e746510dd70
Justin Cheung <jcheung@exeblock.com>
2018-10-10 11:56:50 -0300

    Blockchain length API

--- server/routes/blocks.js
+++ server/routes/blocks.js
@@ -8,19 +8,56 @@ router.get('/healthcheck', function (req, res) {
 	res.send('OK!');
 });
 
+
+// BlockAPI API: GET number of blocks in the blockchain
+router.get('/blocks/length', function (req, res) {
+	console.log('GET request made to /blocks/length');
+
+	const connection = mysql.createConnection({
+		host: db.HOST,
+		user: db.USER,
+		password: db.PASSWORD,
+		database: db.DATABASE
+	});
+
+	// Establish connection
+	connection.connect(function (err) {
+		if (err) {
+			console.error('error connecting to DB: ' + err.stack);
+			return;
+		}
+	});
+
+	const sql = 'SELECT COUNT(*) FROM blocks;';
+
+	// Perform Query
+	connection.query(sql, function (err, rows, fields) {
+		if (err) throw err;
+
+		if (rows) {
+			res.send(rows[0]['COUNT(*)'].toString());
+		} else {
+			res.send('0');
+		}
+	});
+
+	// Close connection
+	connection.end();
+});
+
 // BlockAPI API: GET last block
 router.get('/blocks/last', function (req, res) {
 	console.log('GET request made to /blocks/last');
 
 	const connection = mysql.createConnection({
-		host     : db.HOST,
-		user     : db.USER,
-		password : db.PASSWORD,
-		database : db.DATABASE
+		host: db.HOST,
+		user: db.USER,
+		password: db.PASSWORD,
+		database: db.DATABASE
 	});
 
-		  // Establish connection
-		  connection.connect(function(err) {
+	// Establish connection
+	connection.connect(function (err) {
 		if (err) {
 			console.error('error connecting to DB: ' + err.stack);
 			return;
@@ -36,16 +73,16 @@ router.get('/blocks/last', function (req, res) {
 	// Perform Query
 	connection.query(sql, function (err, rows, fields) {
 		if (err) throw err;
-	
+
 		if (rows.length < 1) {
 			res.status(400).send('400 NO DATA AVAILABLE');
-			connection.end();	
+			connection.end();
 			return;
 		}
-			  
+
 		res.send(rows);
-			  });
-	
+	});
+
 	// Close connection
 	connection.end();
 });
@@ -57,7 +94,7 @@ router.get('/blocks', function (req, res) {
 		res.status(400).send('400 Bad Request');
 		return;
 	}
-	
+
 
 	const start = req.query.start;
 	const end = req.query.end;
@@ -66,14 +103,14 @@ router.get('/blocks', function (req, res) {
 
 
 	const connection = mysql.createConnection({
-		host     : db.HOST,
-		user     : db.USER,
-		password : db.PASSWORD,
-		database : db.DATABASE
-		  });
+		host: db.HOST,
+		user: db.USER,
+		password: db.PASSWORD,
+		database: db.DATABASE
+	});
 
-		  // Establish connection
-		  connection.connect(function(err) {
+	// Establish connection
+	connection.connect(function (err) {
 		if (err) {
 			console.error('error connecting to DB: ' + err.stack);
 			return;
@@ -104,9 +141,9 @@ router.get('/blocks', function (req, res) {
 			connection.end();
 			return;
 		}
-		  
+
 		res.send(rows);
-		  });
+	});
 
 	// Close connection
 	connection.end();
@@ -117,14 +154,14 @@ router.get('/variables', function (req, res) {
 	console.log('GET request made to /variables');
 
 	const connection = mysql.createConnection({
-		host     : db.HOST,
-		user     : db.USER,
-		password : db.PASSWORD,
-		database : db.DATABASE
-		  });
+		host: db.HOST,
+		user: db.USER,
+		password: db.PASSWORD,
+		database: db.DATABASE
+	});
 
-		  // Establish connection
-		  connection.connect(function(err) {
+	// Establish connection
+	connection.connect(function (err) {
 		if (err) {
 			console.error('error connecting to DB: ' + err.stack);
 			return;
@@ -136,7 +173,7 @@ router.get('/variables', function (req, res) {
 		if (err) throw err;
 		console.log(rows[0]);
 		res.send(rows);
-		  });
+	});
 
 	// Close connection
 	connection.end();
