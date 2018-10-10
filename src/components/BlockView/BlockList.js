a6f220c4bbf9c53664914093fee570b6f4d82cc4
Adrian <ametzler@exeblock.com>
2018-10-10 15:18:28 -0300

    block are now called in line with page number

--- src/components/BlockView/BlockList.js
+++ src/components/BlockView/BlockList.js
@@ -3,13 +3,14 @@ import axios from 'axios';
 import Pagination from 'react-paginate';
 import styles from './styles.css';
 import {Table} from 'reactstrap'; 
+import { connect } from 'react-redux'; 
 
-export default class BlockList extends Component {
+class BlockList extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			blocks: [], lower: 0, upper: 1, currentPage: 1, blockLength: 0
+			blocks: [], currentPage: 0, blockLength: 0
 		};
 		this.blocksPerPage= 20;
 	}
@@ -19,52 +20,37 @@ export default class BlockList extends Component {
 		let upper=1;
 		axios.get('api/blocks/last', {
 		}).then(response => {
-			lower=response.data[0].block_number-100;
+			lower=response.data[0].block_number-(this.blocksPerPage-1);
 			upper=response.data[0].block_number;
 			return axios.get(`api/blocks?start=${lower}&end=${upper}`);
 		}).then(response => {
-			this.setState({blocks: response.data.reverse(), lower, upper, blockLength: Math.ceil(upper/this.blocksPerPage)});
+			this.setState({blocks: response.data.reverse(), lower, upper});
+			return axios.get('api/blocks/length');
+		}).then(response => {
+			this.setState({blockLength: response.data});
 		}).catch(error => console.log('error fetching blocks: ', error));
 	}
 
 	loadNextBlocks(currentPage) {
-		const requestedBlockRange = (this.state.upper-this.blocksPerPage)- (this.blocksPerPage*currentPage);
-		const block = this.state.blocks.findIndex(el => el.block_number === requestedBlockRange);
-		if(block > 0) //block will be > 0 if it exists or -1 if it does not
-		{
-			//make an API call to get the missing data
-		}
-		//if(block)
-		console.log('requested block range: ', requestedBlockRange);
-		//HOW DO I LOAD THE DATA BASED ON PAGE NUMBER
-		//blocks at a time = 100
-		//pages = 11,251
-		//page size = 20
-		//currentPage * pageSize, (currentPage + 1) * pageSize?
-		//if(this.state.blocks[])//request for blocks has exceeded what is in the array
-
-
-		/*
-		axios.get(`api/blocks?start=${this.state.currentBlock+1}&end=${this.upperBound}`, {
+		const requestedBlockRange = this.state.upper - (this.blocksPerPage*currentPage);
+		axios.get(`api/blocks?start=${requestedBlockRange-(this.blocksPerPage-1)}&end=${requestedBlockRange}`, {
 		}).then(response => {
-			this.setState({ blocks: [...this.state.blocks, ...response.data] });
-		}).catch(error => console.log('error fetching blocks'));*/
+			this.setState({ blocks: response.data.reverse() });
+		}).catch(error => console.log('error fetching blocks'));
 	 }
 
 	changePage(index) {
-		this.setState({currentPage: index.selected+1});
-		this.loadNextBlocks(index.selected+1);
+		this.setState({currentPage: index.selected});
+		this.loadNextBlocks(index.selected);
 	}
 
-	// getPageLength() {
-	// 	const test = !!this.state.blocks && this.state.blocks.length > 0 ? Math.ceil(this.state.blocks[0].block_number/this.blocksPerPage) : 1;
-	// 	debugger;
-	// 	return test;
-	// }
+	getWitnessName(witnessId) {
+		return this.props.witnesses.find(el => el.account_id === witnessId).account_name;
+	}
 
 	render() {
-		const {blocks, currentPage, blockLength} = this.state;
-		console.log('blocks: ',blocks);
+		const {blocks, blockLength} = this.state;
+		console.log('blocks: ', this.state.blocks);
 		return (
 			<div className="container pt-4 pb-5 mt-5">
 				<div className="card mt-3">
@@ -81,12 +67,12 @@ export default class BlockList extends Component {
 								</tr>
 							</thead>
 							<tbody className="text-center">
-								{blocks.slice(currentPage * this.blocksPerPage, (currentPage + 1) * this.blocksPerPage).map((block) => {
+								{blocks.map((block) => {
 									return(
 										<tr key={block.id}>
-											<td>{block.block_number}</td>
+											<td><a href="/block-view">{block.block_number}</a></td>
 											<td>{new Date(block.timestamp).toLocaleTimeString()}</td>
-											<td>{block.witness}</td>
+											<td>{this.getWitnessName(block.witness)}</td>
 											<td>{block.transaction_count}</td>
 											<td>{block.operation_count}</td>
 										</tr>
@@ -94,7 +80,6 @@ export default class BlockList extends Component {
 								})}
 							</tbody>
 						</Table>
-						{/* <PaginationCall currentPage={currentPage} handleClick={this.changePage.bind(this)} pagesCount={this.getPageLength()} /> */}
 						<Pagination
 							breakClassName={`${styles['pagination']}`}
 							breakLabel={<a className="page-link">...</a>}
@@ -104,7 +89,7 @@ export default class BlockList extends Component {
 							pageLinkClassName="page-link"
 							previousLinkClassName="page-link"
 							nextLinkClassName="page-link"
-							pageCount={blockLength}
+							pageCount={blockLength/this.blocksPerPage}
 							pageRangeDisplayed={2}
 							onPageChange={this.changePage.bind(this)}
           				/>
@@ -114,4 +99,10 @@ export default class BlockList extends Component {
 		);
 	}
 }
+
+const mapStateToProps = (state) => ({
+	witnesses: state.witnesses.witnessList
+});
+
+export default connect(mapStateToProps)(BlockList);
 
