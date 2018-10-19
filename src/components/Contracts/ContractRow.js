d05a4ac55380bcb083f158b612f04ef9469a5a48
Prabhjot Singh <psingh@exeblock.com>
2018-10-19 10:16:44 -0300

    Minor fix

--- src/components/Contracts/ContractRow.js
+++ src/components/Contracts/ContractRow.js
@@ -7,13 +7,13 @@ class ContractRow extends Component {
 		const balance = JSON.parse(this.props.detail.balances);
 		return (
 			<tr>
-				<td><NavLink tag={RRNavLink} to={'/contractDetail/'+this.props.detail.name} >{ this.props.detail.object_id }</NavLink></td>
-				<td>{ this.props.detail.name}</td>
-				<td> 
+				<td className="align-middle"><NavLink tag={RRNavLink} to={'/contractDetail/'+this.props.detail.name} >{ this.props.detail.object_id }</NavLink></td>
+				<td className="align-middle">{ this.props.detail.name}</td>
+				<td className="align-middle"> 
 					{ balance && balance.map((bal, i) =>{
 						const str = 'EXE ' + bal.amount;
 						return (
-							<Row>
+							<Row key={i}>
 								<div>{str}</div>
 							</Row>
 						);
