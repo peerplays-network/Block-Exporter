44f221aa0d1abfd35d04e51f1f7df34e18be8430
Justin Cheung <jcheung@exeblock.com>
2018-10-24 11:26:43 -0300

    no message

--- server/routes/transactions.js
+++ server/routes/transactions.js
@@ -40,7 +40,47 @@ router.get('/transactions/:id', function (req, res) {
 	connection.end();
 });
 
-// BlockAPI API: GET transactions range
+// Transactions API: GET recent transactions
+router.get('/transactions/recent', function (req, res) {
+	// Start and End are required.
+	if (!req.query.start || !req.query.end) {
+		res.status(400).send('400 Bad Request');
+		return;
+	}
+
+	const connection = mysql.createConnection({
+		host     : db.HOST,
+		user     : db.USER,
+		password : db.PASSWORD,
+		database : db.DATABASE
+	});
+
+		  // Establish connection
+	connection.connect(function(err) {
+		if (err) {
+			console.error('error connecting to DB: ' + err.stack);
+			return;
+		}
+	});
+
+	const sql = `SELECT * FROM explorer.transactions WHERE id <= ${req.query.id} LIMIT ${req.query.limit}`;
+
+	// Perform Query
+	connection.query(sql, function (err, rows, fields) {
+		if (err) throw err;
+        
+		if (rows.length < 1) {
+			return res.status(404).send('404 NO DATA AVAILABLE - CHECK YOUR PARAMS');
+		}
+		  
+		res.send(rows);
+		  });
+
+	// Close connection
+	connection.end();
+});
+
+// Transactions API: GET transactions range
 router.get('/transactions', function (req, res) {
 	// Start and End are required.
 	if (!req.query.start || !req.query.end) {
