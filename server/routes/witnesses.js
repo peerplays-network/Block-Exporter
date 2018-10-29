ad0e0622d314bfaec1525ced74350ba1016dd231
Justin Cheung <jcheung@exeblock.com>
2018-10-29 11:57:34 -0300

    get api/committee

--- server/routes/witnesses.js
+++ server/routes/witnesses.js
@@ -35,6 +35,54 @@ router.get('/committee/:id', function (req, res) {
 	connection.end();
 });
 
+// Committee API: List all committee members
+router.get('/committee', function (req, res) {
+	const colNames = ['id', 'committee_id', 'committee_member_account', 'vote_id', 'total_votes', 'url'];
+
+	const connection = mysql.createConnection({
+		host     : db.HOST,
+		user     : db.USER,
+		password : db.PASSWORD,
+		database : db.DATABASE
+		  });
+
+		  // Establish connection
+		  connection.connect(function(err) {
+		if (err) {
+			console.error('error connecting to DB: ' + err.stack);
+			return;
+		}
+	});
+    
+	let sql = 'SELECT * FROM explorer.committee';
+
+	if (req.query.sort) { // Handle sorting and direction
+		if (!colNames.includes(req.query.sort)) {
+			res.status(400).send('400 Bad Request - Invalid sort parameter, shame on you');
+			return;
+		}
+		sql = `SELECT * FROM explorer.committee ORDER BY ${req.query.sort}`;
+		if (req.query.direction) {
+			if (req.query.direction !== 'ASC' && (req.query.direction !== 'DESC')) {
+				res.status(400).send('400 Bad Request - Invalid direction');
+				return;
+			} else {
+				sql = sql + ` ${req.query.direction}`;
+			}
+		}
+	}
+
+	// Perform Query
+	connection.query(sql, function (err, rows, fields) {
+		if (err) throw err;
+		  
+		res.send(rows);
+		  });
+
+	// Close connection
+	connection.end();
+});
+
 // Witness API: GET specific witness
 router.get('/witnesses/:name', function (req, res) {
 	const connection = mysql.createConnection({
