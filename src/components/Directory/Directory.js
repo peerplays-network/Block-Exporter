08ff46b0789e748000d9979fc339dc7c122572ea
admin <admin@AlexandersMBP3.citywide.eastlink.ca>
2018-11-06 15:24:21 -0400

    adding the API to the resources table and creating a better formatted result

--- src/components/Directory/Directory.js
+++ src/components/Directory/Directory.js
@@ -1,27 +1,75 @@
 import React,  { Component } from 'react';
-import { NavLink, Card, CardBody, Col } from 'reactstrap';
-import { NavLink as RRNavLink } from 'react-router-dom';
+import { Button, Card, CardText, CardBody, Col } from 'reactstrap';
 import styles from './styles.css';
+import axios from 'axios';
 
 class Directory extends Component {
-    
+	constructor() {
+		super();
+		this.state = {
+			resources: [],
+			categories:[],
+		};
+	}
+
+	componentDidMount() {
+		this.fetchData();
+	}
+
+	fetchData() {
+		axios.get('/api/resources', {
+		}).then(response => {
+			var resources = response.data;
+			var categories = this.state.categories;
+			categories.push(resources.map((resource, i) => {
+				if(!categories.includes(resource.category)) {
+					return resource.category;
+				}
+			}));
+			var catgo = categories[0].filter((v, i) => categories[0].indexOf(v) === i);
+			console.log('out loop', resources, categories, catgo);
+			this.setState({ resources: resources});
+			this.setState({ categories: catgo});
+		}).catch(error => console.log('error fetching transactions', error));
+	}
+
+	renderResource(resource, i) {
+		return( 
+			<Card key={i}>
+				<CardBody>
+					<Button href={resource.url} active>{resource.name}</Button>
+					<CardText>{resource.description}</CardText>
+				</CardBody>
+			</Card>
+		);
+	}
+
+	renderResources(category, resources) {
+		return resources.map((resource, i) => {
+			if(resource.category===category) {
+				return this.renderResource(resource, i);
+			}
+		});
+	}
+
 	render() {
+		const {resources, categories } = this.state;
+
 		return (
 			<div className="container">
-
 				<h1 className={`${styles['header-contrast-text']} ${styles['header-background']} display-5 text-center pt-2 pb-3 mt-5`}>
 					<span className="fa fa-map-signs">&nbsp;</span>Useful Resources</h1>
-
 				<Col >
-					<Card>
-						<CardBody>
-							<NavLink tag={RRNavLink} to="/account/">Account &nbsp; - A list of current accounts in the database</NavLink>
-							<NavLink tag={RRNavLink} to="/feeDirectory">Fee Schedule &nbsp; - A list of current fees applied to accounts</NavLink>
-							<NavLink tag={RRNavLink} to="/">Home &nbsp; - Landing page with access to all widgets</NavLink>
-							<NavLink tag={RRNavLink} to="/transactions/">Transactions &nbsp; - List of the most recent transactions</NavLink>
-							<NavLink tag={RRNavLink} to="test/">Test &nbsp; - A test page</NavLink>
-						</CardBody>
-					</Card>
+					{categories.map((category, i) => {
+						return ( 
+							<div key="i"> 
+								<h1 className={'display-5 text-center pt-2 pb-3 mt-5'}>
+									<span>&nbsp;</span> {category}
+								</h1>
+								{this.renderResources(category, resources, i)}
+							</div>
+						);
+                  	})}
 				</Col>
 			</div>
 		);
