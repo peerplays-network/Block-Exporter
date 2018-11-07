a8fceb06c052c91d7cdfde5768f900762801ca69
Justin Cheung <jcheung@exeblock.com>
2018-11-07 10:55:04 -0400

    contract bal sort

--- server/routes/contracts.js
+++ server/routes/contracts.js
@@ -66,13 +66,18 @@ router.get('/contracts', function (req, res) {
 			return;
 		}
 		sql = `SELECT * FROM explorer.contracts ORDER BY LENGTH(${req.query.sort}) ASC, ${req.query.sort} ASC`; // Natural Sort
-		// sql = `SELECT * FROM explorer.contracts ORDER BY ${req.query.sort}`;
+		if (req.query.sort == 'balances') {
+			sql = `SELECT * FROM explorer.contracts ORDER BY ${req.query.sort}`;
+		}
 		if (req.query.direction) {
 			if (req.query.direction !== 'ASC' && (req.query.direction !== 'DESC')) {
 				res.status(400).send('400 Bad Request - Invalid direction');
 				return;
 			} else {
 				sql = `SELECT * FROM explorer.contracts ORDER BY LENGTH(${req.query.sort}) ${req.query.direction}, ${req.query.sort} ${req.query.direction}`; // NATURAL SORT;
+				if (req.query.sort == 'balances') {
+					sql = `SELECT * FROM explorer.contracts ORDER BY ${req.query.sort} ${req.query.direction}`;
+				}
 			}
 		}
 	}
